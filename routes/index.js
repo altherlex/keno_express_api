6ab19c31786b76d22fb7f267d06133744fd754f7
Altherlex Alves <para.alves@gmail.com>
2018-06-30 20:57:53 -0300

    new title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Keno API' });
+  res.render('index', { title: 'Purchasing Cryptocurrency App' });
 });
 
 module.exports = router;
